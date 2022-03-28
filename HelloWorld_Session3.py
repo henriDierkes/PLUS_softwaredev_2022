81dcce88cbfc2a7d27dda13c32d52bcbf67075be
Henrike <(!C0mP7t38223>
2022-03-28 02:23:12 -0700

    Session 3 Spyder Test

---
+++ HelloWorld_Session3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 28 02:15:40 2022
+
+@author: Henrike
+"""
+
+import os
+import sys
+
+# add a function and text
+print(sys.path)
+help("modules")
+days_of_the_week = 7
+def say_hello(recipient):
+    print("Hello, world! Hello{}".format(recipient))
+    return recipient
+
